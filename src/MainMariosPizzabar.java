1cd436d835bdf669bc217a94852d94e9d4884374
Thom719c <thom719c@stud.kea.dk>
2021-10-26 13:39:25 +0200

    Switch

--- src/MainMariosPizzabar.java
+++ src/MainMariosPizzabar.java
@@ -1,9 +1,21 @@
+import java.util.*;
+
 public class MainMariosPizzabar {
     public static void main(String[] args) {
         runMariosPizzaProgram();
     }
 
     public static void runMariosPizzaProgram() {
-
+        Scanner input = new Scanner(System.in);
+        while (true){
+            int menuChoich = input.nextInt();
+            switch (menuChoich){
+                case 1:
+                case 5:
+                    System.exit(0);
+                default:
+                    System.out.println();
+            }
+        }
     }
 }
