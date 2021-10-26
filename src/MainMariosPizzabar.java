2e0640e1849a2eb81de2fbde48d1b69e8195aee5
Thom719c <thom719c@stud.kea.dk>
2021-10-26 11:36:47 +0200

    Marios Pizzabar

---
+++ src/MainMariosPizzabar.java
@@ -0,0 +1,9 @@
+public class MainMariosPizzabar {
+    public static void main(String[] args) {
+        runMariosPizzaProgram();
+    }
+
+    public static void runMariosPizzaProgram() {
+
+    }
+}
