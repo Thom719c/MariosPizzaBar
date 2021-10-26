9bf0ea30e30df746f0d8560960dfb0af4b6d33c7
JosefineNelbom <josefine.r.nelbom@gmail.com>
2021-10-26 15:07:40 +0200

    ny

--- src/Bestilling.java
+++ src/Bestilling.java
@@ -1,4 +1,18 @@
-public class Bestilling {
+import java.util.*;
 
+public class Bestilling {
+    public void bestilling(){
+        Scanner input = new Scanner(System.in);
+        tilfojBestilling(input);
+
+    }
+    public void tilfojBestilling(Scanner input){
+        System.out.println("Indtast pizzanummer: ");
+        int pizzaNr;
+        int answer = 0;
+        do{
+         pizzaNr = input.nextInt();
+        }while(answer != 0);
+    }
 
 }
