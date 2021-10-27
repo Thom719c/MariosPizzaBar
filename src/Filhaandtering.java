0d817f4a4aa0a8ef5fac45d85ff38f95b5f9bfe4
xenius <xeni0103@stud.kea.dk>
2021-10-27 14:33:39 +0200

    ...

--- src/Filhaandtering.java
+++ src/Filhaandtering.java
@@ -10,11 +10,36 @@ public class Filhaandtering {
         }
     }
     public void readB(String[] bestillingsliste)throws FileNotFoundException{
-        Scanner readBScanner = new Scanner(new File("Ressources/Bestillingsliste"));
+        Scanner input = new Scanner(new File("Ressources/Bestillingsliste"));
+        PrintStream output = new PrintStream(new File("Ressources/Bestillingsliste"));
+
+        String[] saveList = new String[10];
+
+        boolean test = true;
+        int count = 0;
+        while(test) {
+            if(input.hasNextLine()){
+                saveList[count] = input.nextLine();
+                count++;
+            }
+            if(!input.hasNextLine()){
+                test = false;
+            }
+        }
+        String outBestillingsliste = "";
+        for(int i = 0; i <= saveList.length - 1; i++){
+            outBestillingsliste += saveList[i];
+        }
+        output.println(outBestillingsliste);
+
+        System.out.println(outBestillingsliste);
+
+
+        /*
         int tempI = 0;
         while(readBScanner.hasNext()) {
             System.out.printf("%3d. %s\n", tempI,readBScanner.nextLine());
             tempI++;
-        }
+        }*/
     }
 }
