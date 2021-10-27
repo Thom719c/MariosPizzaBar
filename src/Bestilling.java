36861e1a2e9a8059c2cf1cc0f7ddd1074cd7310c
PhilipNS98 <90253330+PhilipNS98@users.noreply.github.com>
2021-10-27 12:58:45 +0200

    Added readB method in Filhaandtering Added fjernBestilling in Bestilling

--- src/Bestilling.java
+++ src/Bestilling.java
@@ -3,18 +3,20 @@ import java.util.*;
 
 public class Bestilling {
     private String[] bestillingsliste = new String[10];
+
     public void bestilling() throws FileNotFoundException {
         Scanner input = new Scanner(System.in);
         Filhaandtering filhaandtering = new Filhaandtering();
-        //tilfojBestilling(input);
+        tilfojBestilling(input);
         fjernBestilling(input, filhaandtering);
 
     }
-    public void tilfojBestilling(Scanner input){
+
+    public void tilfojBestilling(Scanner input) {
         //System.out.println("Indtast pizzanummer: ");
         int tempI = 0;
         System.out.println("Indtast pizzanummer (f.eks: 1, 5, 3): ");
-        while (true){
+        while (true) {
             String pizzaNr = input.nextLine();
             if (!pizzaNr.equals("")) {
                 bestillingsliste[tempI] = pizzaNr;
@@ -24,13 +26,16 @@ public class Bestilling {
         }
         System.out.println(Arrays.toString(bestillingsliste));
     }
-    public void fjernBestilling(Scanner input, Filhaandtering filhaandtering) throws FileNotFoundException{
+
+    public void fjernBestilling(Scanner input, Filhaandtering filhaandtering) throws FileNotFoundException {
         System.out.println("Bestillingslisten:");
         filhaandtering.readB(bestillingsliste);
+        int tempI = 1;
         System.out.println("\nHvilken bestilling vil du slette?:");
-           int bestillingNr = input.nextInt();
-           bestillingsliste[bestillingNr-1] = "";
+        int number = input.nextInt();
+        if(number >= 1 && number <= bestillingsliste.length){
+            bestillingsliste[number-1] = null;
+        }
         System.out.println(Arrays.toString(bestillingsliste));
-           //writetofile
     }
 }
--- src/Filhaandtering.java
+++ src/Filhaandtering.java
@@ -9,11 +9,14 @@ public class Filhaandtering {
             tempI++;
         }
     }
-    public void readB(String[] bestillingsliste)throws FileNotFoundException{
+    public void readB(String[] bestillingNr)throws FileNotFoundException{
         Scanner readBScanner = new Scanner(new File("Ressources/Bestillingsliste"));
+        //String[] bestillingNr = new String[10];
         int tempI = 0;
         while(readBScanner.hasNext()) {
-            System.out.printf("%3d. %s\n", tempI,readBScanner.nextLine());
+            //System.out.printf("%3d. %s\n", tempI,readBScanner.nextLine());
+            bestillingNr[tempI] = readBScanner.nextLine();
+            System.out.println((tempI+1) + ". " + bestillingNr[tempI]);
             tempI++;
         }
     }
