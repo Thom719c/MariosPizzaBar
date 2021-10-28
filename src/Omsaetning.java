67e5c241ecb5001cb2604548ae64539c8fc62d82
Thom719c <thom719c@stud.kea.dk>
2021-10-28 13:16:32 +0200

    -

--- src/Omsaetning.java
+++ src/Omsaetning.java
@@ -1,3 +1,11 @@
 public class Omsaetning {
+    private Filhaandtering filhaandtering = new Filhaandtering();
+    private Bestilling bestilling = new Bestilling();
 
+    public Omsaetning() {
+    }
+
+    public void beregnPris() {
+        int[] countNr = new int[14];
+    }
 }
