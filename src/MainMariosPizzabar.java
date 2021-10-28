e8f32fba6bfed5002f674bf848005ca5a66c00bf
Thom719c <thom719c@stud.kea.dk>
2021-10-28 23:13:29 +0200

    Lavet omsætning hvor den kan lægger det ud i en fil. Tilrettet tilføj Bestilling så der kommer pris og afhentning tidspunt ud, samt hvilke pizza nummer og hvor mange af dem.

--- src/MainMariosPizzabar.java
+++ src/MainMariosPizzabar.java
@@ -10,10 +10,12 @@ public class MainMariosPizzabar {
     public static void runMariosPizzaProgram() throws FileNotFoundException {
         Filhaandtering filhaandtering = new Filhaandtering();
         Bestilling bestilling = new Bestilling();
+        Omsaetning omsaetning = new Omsaetning();
         Scanner input = new Scanner(System.in);
         while (true){
-            System.out.println("Menu - 1-5 \n" + "1. Menukort \n2. Vis bestillings menu \n3. Vis omsætning\n4. For exit" + "\nIndtast nummer: ");
+            System.out.println("Menu - 1-5" + "\n1. Menukort \n2. Vis bestillings menu \n3. Vis omsætning\n4. For exit" + "\nIndtast nummer: ");
             int menuChoice = input.nextInt();
+            System.out.println();
             switch (menuChoice){
                 case 1:
                     filhaandtering.readFil();
@@ -23,7 +25,7 @@ public class MainMariosPizzabar {
                     bestilling.bestilling();
                     break;
                 case 3:
-
+                    filhaandtering.readOmsaetning();
                     break;
                 case 4:
                     System.exit(0);
