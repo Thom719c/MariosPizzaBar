a20bec9cb65c63e668c94a06f45945cce52a5d9b
Thom719c <thom719c@stud.kea.dk>
2021-10-29 10:34:58 +0200

    Opdater omsætning

--- src/MainMariosPizzabar.java
+++ src/MainMariosPizzabar.java
@@ -10,9 +10,11 @@ public class MainMariosPizzabar {
     public static void runMariosPizzaProgram() throws FileNotFoundException {
         Filhaandtering filhaandtering = new Filhaandtering();
         Bestilling bestilling = new Bestilling();
+        Omsaetning omsaetning = new Omsaetning();
         Scanner input = new Scanner(System.in);
         while (true){
-            System.out.println("Menu - 1-5" + "\n1. Menukort \n2. Vis bestillings menu \n3. Vis omsætning\n4. For exit" + "\nIndtast nummer: ");
+            System.out.println("Menu - 1-4" +
+                    "\n1. Menukort \n2. Vis bestillings menu \n3. Vis salgsstatistik- og omsætnings menu \n4. For exit" + "\nIndtast nummer: ");
             int menuChoice = input.nextInt();
             System.out.println();
             switch (menuChoice){
@@ -24,7 +26,7 @@ public class MainMariosPizzabar {
                     bestilling.bestilling();
                     break;
                 case 3:
-                    filhaandtering.readOmsaetning();
+                    omsaetning.omsaetning();
                     break;
                 case 4:
                     System.exit(0);
--- src/Omsaetning.java
+++ src/Omsaetning.java
@@ -4,8 +4,32 @@ import java.util.*;
 
 public class Omsaetning {
     private Filhaandtering filhaandtering = new Filhaandtering();
-    private int[] count = new int[15];
-    private int[] prisCount = new int[15];
+    private int[] count = new int[14];
+    private int[] prisCount = new int[14];
+
+    public void omsaetning() throws FileNotFoundException {
+        Scanner input = new Scanner(System.in);
+        int menuOmsaetningChoice = 0;
+        while (menuOmsaetningChoice != 4){
+            System.out.println("Omsætning menu" +
+                    "\n1. Vis salgsstatistik \n2. Vis omsætning \n3. Ryd salgsstatistik og omsætning \n4. For exit" + "\nIndtast nummer: ");
+            menuOmsaetningChoice = input.nextInt();
+            System.out.println();
+            switch (menuOmsaetningChoice){
+                case 1:
+                    filhaandtering.readSalgsstatistik();
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                default:
+                    System.out.println();
+            }
+        }
+    }
 
     public void filPizzaCounter(int line)throws FileNotFoundException {
         filhaandtering.opdaterOmsaetning(count);
@@ -22,19 +46,14 @@ public class Omsaetning {
         String[] arrOfStr = antalBestil.split(",");               //Deler den String op ved hvert komma og sætter det ind på en plads i array
 
         for (int i = 0; i < arrOfStr.length; i++){
-            for (int j = 0; j <= 14; j++) {
-                if (arrOfStr[i].equals(String.valueOf(j))){             //Tjekker om der er den værdi i array og hvis der er kan den gå ind og tælle count op
-                    count[j-1]++;
+            for (int j = 0; j < 14; j++) {
+                if (arrOfStr[i].equals(String.valueOf(j+1))){             //Tjekker om der er den værdi i array og hvis der er kan den gå ind og tælle count op
+                    count[j]++;
                     break;
                 }
             }
         }
-        for (int i = 0; i <= 14; i++) {
-            if (count[i] >= 1){
-                System.out.printf("Pizzanummer: %d antal bestillinger %d\n", (i+1), count[i]);
-            }
-        }
-        filhaandtering.writeToOmsaetning(count);
+        filhaandtering.writeToSalgsstatistik(count);
     }
 
     public void pizzaPris(String bestillingsliste, String tid)throws FileNotFoundException {
@@ -43,9 +62,9 @@ public class Omsaetning {
         String[] arrOfStr = antalBestil.split(",");
 
         for (int i = 0; i < arrOfStr.length; i++){
-            for (int j = 0; j <= 14; j++) {
-                if (arrOfStr[i].equals(String.valueOf(j))){
-                    prisCount[j-1]++;
+            for (int j = 0; j < 14; j++) {
+                if (arrOfStr[i].equals(String.valueOf(j+1))){
+                    prisCount[j]++;
                     break;
                 }
             }
@@ -56,13 +75,13 @@ public class Omsaetning {
         filhaandtering.pizzaPris();
         int[] pris = new int[10];
         int tempPris = 0;
-        for (int i = 0; i <= 14; i++) {
+        for (int i = 0; i < 14; i++) {
             if (prisCount[i] >= 1){
                 System.out.printf("Pizzanummer: %d antal bestillinger %d \n", (i+1), prisCount[i]);
                 tempPris += prisCount[i] * filhaandtering.priser[i];
             }
         }
-        for (int i = 0; i <= 14; i++){
+        for (int i = 0; i < 14; i++){
             if (pris[i] == 0) {
                 pris[i] = tempPris;
                 System.out.printf("Pris for bestillingen: %d kr \nAfhentningstidspunkt: %4s\n\n", pris[i], tid);
