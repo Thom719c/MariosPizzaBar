4dcbfc364fd5efc718488062db4294d56db3650c
Thom719c <thom719c@stud.kea.dk>
2021-10-31 17:45:46 +0100

    Opdater tilføj bestilling

--- src/Bestilling.java
+++ src/Bestilling.java
@@ -12,12 +12,12 @@ public class Bestilling {
         sorterAfhentingstidpunkt();
         Scanner input = new Scanner(System.in);
         int bestillingChoice = 0;
-        while (bestillingChoice != 4){
+        while (bestillingChoice != 4) {
             System.out.println("Bestilling menu" +
                     "\n1. Tilføj bestilling \n2. Fjern bestilling \n3. Vis bestillingsliste \n4. For exit" + "\nIndtast nummer: ");
             bestillingChoice = input.nextInt();
             input.nextLine();
-            switch (bestillingChoice){
+            switch (bestillingChoice) {
                 case 1:
                     tilfojBestilling(input);
                     break;
@@ -32,21 +32,20 @@ public class Bestilling {
             }
         }
     }
+
     public void tilfojBestilling(Scanner input) throws FileNotFoundException {
         printBestillinger();
 
-        for (int i = 0; i < bestillingsliste.length; i++){
+        for (int i = 0; i < bestillingsliste.length; i++) {
             if (bestillingsliste[i].equals("null")) {
                 System.out.println("Indtast pizzanummer (f.eks: 1, 5, 3): ");
                 String pizzaNr = input.nextLine();
                 System.out.println("Indtast afhentningstidspunkt: ");
                 String afhentningstidspunkt = input.next();
                 bestillingsliste[i] = pizzaNr + ". " + afhentningstidspunkt;
-                //  System.out.println("Bestilling tilføjet: " + bestillingsliste[i] + "\n");
 
                 String result = bestillingsliste[i];
-                String[] split = result.split( Pattern.quote( "." ) );
-                String tid = split[split.length-1];
+                String tid = result.substring(result.indexOf(".") + 1);
 
                 salgsstatistikOmsaetning.pizzaPris(bestillingsliste[i], tid);
                 break;
@@ -60,32 +59,38 @@ public class Bestilling {
         System.out.println("\nHvilken bestilling vil du slette?:");
         int number = input.nextInt();
         salgsstatistikOmsaetning.filPizzaCounter(number);
-        if(number >= 1 && number <= bestillingsliste.length){
-            bestillingsliste[number-1] = "null";
+        if (number >= 1 && number <= bestillingsliste.length) {
+            bestillingsliste[number - 1] = "null";
         }
         printBestillinger();
     }
+
     public void printBestillinger() throws FileNotFoundException {
         sorterAfhentingstidpunkt();
         System.out.println("Bestillingslisten:");
-        for (int i = 0; i < bestillingsliste.length; i++){
-            System.out.printf("Nr %2d: %s\n", (i+1), bestillingsliste[i]);
+        for (int i = 0; i < bestillingsliste.length; i++) {
+            if (bestillingsliste[i].contains("null")) {
+                break;
+            }
+            System.out.printf("Nr %2d: %s\n", (i + 1), bestillingsliste[i]);
         }
         System.out.println();
     }
 
+    /**
+     *  Her sorter den efter afhentningstidspunkt
+     */
     public void sorterAfhentingstidpunkt() throws FileNotFoundException {
         String[] afhentningstidspunkt = new String[10];
         String tempBestilling;
         for (int i = 0; i < bestillingsliste.length; i++) {
             String result = bestillingsliste[i];
-            String[] split = result.split( Pattern.quote( "." ) );
-            String lastOne = split[split.length-1];
-            afhentningstidspunkt[i] = lastOne;
+            String s = result.substring(result.indexOf(".") + 1).trim();
+            afhentningstidspunkt[i] = s;
         }
-        for (int i = 0; i <bestillingsliste.length; i++) {
-            for (int j = i+1; j <bestillingsliste.length; j++) {
-                if( afhentningstidspunkt[i].compareTo(afhentningstidspunkt[j]) > 0) {
+        for (int i = 0; i < bestillingsliste.length; i++) {
+            for (int j = i + 1; j < bestillingsliste.length; j++) {
+                if (afhentningstidspunkt[i].compareTo(afhentningstidspunkt[j]) > 0) {
                     tempBestilling = bestillingsliste[i];
                     bestillingsliste[i] = bestillingsliste[j];
                     bestillingsliste[j] = tempBestilling;
--- src/Filhaandtering.java
+++ src/Filhaandtering.java
@@ -6,41 +6,41 @@ public class Filhaandtering {
     int[] priser = new int[14];
 
     //Læser filen pizzaMenuen og printer den til konsolen
-    public void readFil() throws FileNotFoundException{
+    public void readFil() throws FileNotFoundException {
         Scanner readFileScanner = new Scanner(new File("Ressources/pizzaMenu"));
         int tempI = 1;
-        while(readFileScanner.hasNext()) {
-            System.out.printf("%3d. %s\n", tempI,readFileScanner.nextLine());
+        while (readFileScanner.hasNext()) {
+            System.out.printf("%3d. %s\n", tempI, readFileScanner.nextLine());
             tempI++;
         }
     }
 
     //Læser filen Salgsstatistik og printer den til konsolen
-    public void readSalgsstatistik() throws FileNotFoundException{
+    public void readSalgsstatistik() throws FileNotFoundException {
         Scanner readFileScanner = new Scanner(new File("Ressources/Statistik/Salgsstatistik.txt"));
-        while(readFileScanner.hasNext()) {
+        while (readFileScanner.hasNext()) {
             System.out.printf("%s\n", readFileScanner.nextLine());
         }
     }
 
     //tager array bestillingsliste og skriver til filen Bestillingsliste
-    public void writeToBestillingsliste(String[] bestillingsliste) throws FileNotFoundException{
+    public void writeToBestillingsliste(String[] bestillingsliste) throws FileNotFoundException {
         PrintStream out = new PrintStream(new File("Ressources/Bestillingsliste"));
         String bestilling = "";
 
-        for(int i = 0; i < bestillingsliste.length; ++i) {
+        for (int i = 0; i < bestillingsliste.length; ++i) {
             bestilling += bestillingsliste[i] + "\n";
         }
         out.println(bestilling);
     }
 
     //tager array salgsstatistikOmsaetning og skriver til filen Salgsstatistik
-    public void writeToSalgsstatistik(int[] salgsstatistikOmsaetning) throws FileNotFoundException{
+    public void writeToSalgsstatistik(int[] salgsstatistikOmsaetning) throws FileNotFoundException {
         PrintStream out = new PrintStream(new File("Ressources/Statistik/Salgsstatistik.txt"));
         String pizzaOmsaetning = "";
 
-        for(int i = 0; i < 14; ++i) {
-            pizzaOmsaetning += "Pizzanummer: " + (i+1) + " antal pizzaer " + salgsstatistikOmsaetning[i] + "\n";
+        for (int i = 0; i < 14; ++i) {
+            pizzaOmsaetning += "Pizzanummer: " + (i + 1) + " antal pizzaer " + salgsstatistikOmsaetning[i] + "\n";
         }
         out.println(pizzaOmsaetning);
     }
@@ -49,7 +49,7 @@ public class Filhaandtering {
     public void opdaterCountsalgsstatistik(int[] countsalgsstatistik) throws FileNotFoundException {
         int i = 0;
         Scanner readOmsaetning = new Scanner(new File("Ressources/Statistik/Salgsstatistik.txt"));
-        while (readOmsaetning.hasNext()){
+        while (readOmsaetning.hasNext()) {
             String tempS = readOmsaetning.next();   //Linje 55, 56, 57 og 58: lægger dataen ind på midlertidige variabler, for at nå den ønskede data
             int tempInt = readOmsaetning.nextInt();
             tempS = readOmsaetning.next();
@@ -63,7 +63,7 @@ public class Filhaandtering {
     public void opdaterArray(String[] bestillingsliste) throws FileNotFoundException {
         int i = 0;
         Scanner readBestillingsliste = new Scanner(new File("Ressources/Bestillingsliste"));
-        while (readBestillingsliste.hasNext()){
+        while (readBestillingsliste.hasNext()) {
             bestillingsliste[i] = readBestillingsliste.nextLine();
             i++;
         }
@@ -76,19 +76,18 @@ public class Filhaandtering {
         Scanner readFileScanner = new Scanner(new File("Ressources/pizzaMenu"));
 
         int tempI = 0;
-        while(readFileScanner.hasNext()) {
+        while (readFileScanner.hasNext()) {
             menuA[tempI] = readFileScanner.nextLine();
             tempI++;
         }
         for (int i = 0; i < menuA.length; i++) {
             String result = menuA[i];
-            String[] split = result.split( Pattern.quote( "." ) );      //Kigger efter punktum og splitter linjen
-            String lastOne = split[split.length-1];                        //Gemmer den sidste del af den nye String (f.eks. "57 kr")
-            String[] splitF = lastOne.split( Pattern.quote( " " ) );    //Kigger efter mellemrum og splitter
-            String firstOne = splitF[splitF.length-2];                     //Fjerner alt efter mellemrummet
-            priser[i] = Integer.parseInt(firstOne);                        //Sætter værdigen ind på arrayet priser på index 'i'
+            //substring af resultat hvor den tag fra det sidst punktum (der bliver plusset med 1 for at tag den sidste punktum væk) og til den ser " kr" hvor den ender med vores int i "pizzaMenu"
+            String newLast = result.substring(result.lastIndexOf(".") + 1, result.indexOf(" kr"));
+            priser[i] = Integer.parseInt(newLast);
         }
     }
+
     public void printTilPizzaOmsaetning(int omsaetningTotalPris) throws FileNotFoundException {
         PrintStream out = new PrintStream(new File("Ressources/Omsaetning/PizzaOmsaetning.txt"));
         out.println("Totalpris: " + omsaetningTotalPris);
